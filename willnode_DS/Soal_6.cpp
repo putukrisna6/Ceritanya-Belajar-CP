9dd5b882d80f0f19f5735e50348c3856cea25a1b
Putu Krisna Andyartha <43901559+putukrisna6@users.noreply.github.com>
2020-10-30 18:26:53 +0700

    Soal 6 Singkatan

--- willnode_DS/Soal_6.cpp
+++ willnode_DS/Soal_6.cpp
@@ -1,5 +1,25 @@
 #include <bits/stdc++.h>
 
+/*  the main idea is to pair the letters of the abbreviation to the letters of the main string
+*   when the letter is paired, search through the main string to search for the next abbreviation letter's pair
+*   while doing so, keep track of the 'count' that should be under K at all times
+*   if count happen to exceed K, give it another chance to pair up
+*   -------------------------------------------------------------------------------------------------------------
+*   how exactly?
+*   when searching for next pair, keep track of another potential pair for the already paired letter
+*   this method is crucial for test case HOLOGY 7 to be correct
+*   the chance is given as long as the potential pair for the already paired letter,
+*   has less than K letters in between to the previous paired letter (pusing ga, gw juga jelasinnya susah)
+*   -------------------------------------------------------------------------------------------------------------
+*   there are two fail states in here
+*   bool broke means that the current attemp to find next pair doesn't work, so it'll attempt to move the curr pair location
+*   bool permanentFail means it is hopeless, as changing the curr pair location will break it and the next pair is still out of reach as well
+*   -------------------------------------------------------------------------------------------------------------
+*   potential problem might arise as I didn't have any reference to work with, mostly because I can't find similar problems
+*   but I hope I have tested this one extensively enough to make sure it should work
+*   potential solution is unknown since I don't know what are the potential problem to begin with
+*/
+
 using namespace std;
 
 int main() {
