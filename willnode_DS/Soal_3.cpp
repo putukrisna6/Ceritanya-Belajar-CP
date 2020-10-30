85d3fada222b777f4ea03abed252e40ac15511a2
Putu Krisna Andyartha <43901559+putukrisna6@users.noreply.github.com>
2020-10-30 18:13:11 +0700

    Soal 3 What You Type isn't What You Get

--- willnode_DS/Soal_3.cpp
+++ willnode_DS/Soal_3.cpp
@@ -2,6 +2,14 @@
 
 using namespace std;
 
+/*  basically you shift the chars by a factor
+*   that factor is 24 times the single number there
+*   for example 1abc, a is shifted by 24 * 1
+*   if the result of that shift push the char out of lower case letter ASCII, just make it cycle back
+*
+*   there should not be any potential problem with this
+*/
+
 bool checkIfNotNum(char a) {
     return a >= 97 && a <= 122;
 }
